29aee33be19cbf2bc7d17a82a34a032905477496
peppegrass <peppegrass@gmail.com>
2020-09-01 12:25:47 +0200

    Create GG1.jl
    Creating first Julia script for PS1

---
+++ ProblemSets/PS1-julia-intro/GG1.jl
@@ -0,0 +1,3 @@
+# Problem Set 1
+# Giuseppe Grasso, Econ PhD student at LISER/University of Luxembourg (https://liser.elsevierpure.com/en/persons/giuseppe-grasso)
+println("Hello, this is my first Julia script!")
