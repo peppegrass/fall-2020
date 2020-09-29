0cc3a16892e6607abbd1fd9b462122cef9648350
peppegrass <peppegrass@gmail.com>
2020-09-29 17:07:51 +0200

    Reviewed PS5 LIVE with Tyler. Recording available on Notability

--- ProblemSets/PS5-ddc/PS5solutions_GG.jl
+++ ProblemSets/PS5-ddc/PS5solutions_GG.jl
@@ -2,6 +2,7 @@
 # Problem set 5 solutions
 # Written by Tyler Ransom
 # Commented by Giuseppe Grasso
+# Recording available in Notability
 #=~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~=#
 
 using Random
@@ -86,7 +87,8 @@ function allwrap()
     use Optim with your own log likelihood function.)
     =#
 
-    #GG: FILL
+    θ̂_glm = glm(@formula(Y ~ Odometer + Branded), df_long, Binomial(), LogitLink()) # GG: estimating using GLM package. Pooled binary logit using reshaped long data
+    println(θ̂_glm)
 
 #= GG: III - DYNAMIC ESTIMATION
 Now I will walk you through how to estimate the DYNAMIC version of this model using BACKWARDS RECURSION.
@@ -149,17 +151,22 @@ I will walk you through speciﬁc steps for how to do this
     # [ Load in the data ]
     url = "https://raw.githubusercontent.com/OU-PhD-Econometrics/fall-2020/master/ProblemSets/PS5-ddc/busdata.csv"
     df = CSV.read(HTTP.get(url).body)
-
     Y = Matrix(df[:,[:Y1,:Y2,:Y3,:Y4,:Y5,:Y6,:Y7,:Y8,:Y9,:Y10,:Y11,:Y12,:Y13,:Y14,:Y15,:Y16,:Y17,:Y18,:Y19,:Y20]])
+    X = Matrix(df[:,[:Odo1,:Odo2,:Odo3,:Odo4,:Odo5,:Odo6,:Odo7,:Odo8,:Odo9,:Odo10,:Odo11,:Odo12,:Odo13,:Odo14,:Odo15,:Odo16,:Odo17,:Odo18,:Odo19,:Odo20]])
+    Z = Vector(df[:,:RouteUsage])
+    B = Vector(df[:,:Branded])
+    N = size(Y,1)
+    T = size(Y,2)
+    Xstate = Matrix(df[:,[:Xst1,:Xst2,:Xst3,:Xst4,:Xst5,:Xst6,:Xst7,:Xst8,:Xst9,:Xst10,:Xst11,:Xst12,:Xst13,:Xst14,:Xst15,:Xst16,:Xst17,:Xst18,:Xst19,:Xst20]])
+    Zstate = Vector(df[:,:Zst])
 
-    # [ convert other data frame columns to matrices]
-    #GG: FILL
 
     #:::::::::::::::::::::::::::::::::::::::::::::::::::
     # Question 3b: Generate state transition matrices
     # Construct the state transition matrices, which are the f_j’s in (3). To do so, simply run the following code:
     #:::::::::::::::::::::::::::::::::::::::::::::::::::
-    zval,zbin,xval,xbin,xtran = create_grids()
+    zval,zbin,xval,xbin,xtran = create_grids() # GG: creates grid points based on points given. See .jl file pf function
+    # GG: returns bin numbers and associated values for z (rout usage) and x (odometer reading); and transition values for the X
 
     #=
     GG: zval and xval are the grids deﬁned at the bottom of p. 2, which respectively correspond to the route usage
@@ -170,8 +177,6 @@ I will walk you through speciﬁc steps for how to do this
     Note: A Markov transition matrix is a matrix where each row sums to 1 and moving from e.g. column 1 to column 4
     within a row gives the probability of moving from state 1 to state 4. Check out the Wikipedia page for more information
     =#
-
-    #GG: FILL
 
     #:::::::::::::::::::::::::::::::::::::::::::::::::::
     # Question 3c: Compute the future value terms for all possible states of the model
@@ -206,20 +211,75 @@ I will walk you through speciﬁc steps for how to do this
     #     Remember to set β = 0.9
     #:::::::::::::::::::::::::::::::::::::::::::::::::::
 
-    #:::::::::::::::::::::::::::::::::::::::::::::::::::
-    # Question 3d: Construct the log likelihood using the future value terms from the previous step and only
-    # using the observed states in the data. This will entail a for loop over buses and time periods.
-    #:::::::::::::::::::::::::::::::::::::::::::::::::::
-    # • Initialize the log likelihood value to be 0. (We will iteratively add to it as we loop over observations in the data)
-    # • Create a variable that indexes the state transition matrix rows for the case where the bus has been replaced.
-    #   This will be the same 1+(z-1)*xbin as in the conditional value function v 0t above. However, we need to plug in :Zst from the data rather than a hypothetical value z.
-    # • Create a variable that indexes the state transition matrix rows for the case where the bus has not been replaced.
-    #   This will be the same x + (z-1)*xbin as in v1t  above, except we substitute :Xst and :Zst for x and z.
-    # • Now create the ﬂow utility component of v_{1t} − v_{0t} using the actual observed data on mileage and branding.
-    # • Next, we need to add the appropriate discounted future value to round out our calculation of v_{1t} − v_{0t}.
-    #   Here, we can difference the f_j’s as in (5). You should get something like (xtran[row1,:].-xtran[row0,:])'*FV[row0:row0+xbin-1,B[i]+1,t+1]
-    # • Finally, create the choice probabilities for choosing each option as written in (7) and then create the
-    #   log likelihood according to the summation in (6).
+    @views @inbounds function likebus(α,β,Y,B,N,T,X,Z,Zstate,Xstate,xtran,zbin,xbin,xval)
+
+        # FV in period T+1 = 0 since this is a finite horizon problem
+        FV=zeros(zbin*xbin,2,T+1) # GG: initialize future values; initialize the future value array, which should be a 3-dimensional array of zeros.
+        # GG: 2 columns is Branded=1 vs Branded=0
+
+        # First loop: solve the backward recursion problem given the values of α
+        # This will give the future value for *all* possible states that we might visit
+        # This is why the FV array does not have an individual dimension
+        for t=T:-1:1  # GG: Loop backwards over t from T+1 to 1; going backwards over time every period
+            for b=0:1   # GG: Loop over the two possible brand states {0,1}; based on branded or not
+                for z=1:zbin # GG: Loop over the possible permanent route usage states (i.e. from 1 to zbin); which bin am I in the x2 state variable
+                    for x=1:xbin # GG: Loop over the possible odometer states (i.e. from 1 to xbin); which hin am I in the X1t state variable
+                        # inputs to FV
+
+                        # GG: object that marks the row of the transition matrix; to figurate the transition probabilities
+                        row = x + (z-1)*xbin # which row of xtran should we look at? depends on milage bin x and route usage z
+
+                        # GG: conditional value function for driving the bus (v_{1t})
+
+                        v1  = α[1] + α[2]*xval[x] + α[3]*b + xtran[         row,:]⋅FV[(z-1)*xbin+1:z*xbin,b+1,t+1] # mileage bin is x, route usage z is permanent
+                        # GG: flow utility component + summation over state transitions
+                        # GG: set of probs xtran[row,:] DOT PRODUCT Future Values (FV)
+
+                        # GG: conditional value function for replacing the engine (v_{0t})
+                        v0  =                                xtran[1+(z-1)*xbin,:]⋅FV[(z-1)*xbin+1:z*xbin,b+1,t+1] # the engine got replaced => mileage is 0, so first bin
+
+                        # FV is discounted log sum of the exponentiated conditional value functions #GG: Updating Future Value terms!!!
+                        FV[row,b+1,t] = β*log(exp(v1) + exp(v0))
+                        #GG: that's where the recursion comes in!
+                    end
+                end
+            end
+        end
+
+        #:::::::::::::::::::::::::::::::::::::::::::::::::::
+        # Question 3d: Construct the log likelihood using the future value terms from the previous step and only
+        # using the observed states in the data. This will entail a for loop over buses and time periods.
+        #:::::::::::::::::::::::::::::::::::::::::::::::::::
+        # • Initialize the log likelihood value to be 0. (We will iteratively add to it as we loop over observations in the data)
+        # • Create a variable that indexes the state transition matrix rows for the case where the bus has been replaced.
+        #   This will be the same 1+(z-1)*xbin as in the conditional value function v 0t above. However, we need to plug in :Zst from the data rather than a hypothetical value z.
+        # • Create a variable that indexes the state transition matrix rows for the case where the bus has not been replaced.
+        #   This will be the same x + (z-1)*xbin as in v1t  above, except we substitute :Xst and :Zst for x and z.
+        # • Now create the ﬂow utility component of v_{1t} − v_{0t} using the actual observed data on mileage and branding.
+        # • Next, we need to add the appropriate discounted future value to round out our calculation of v_{1t} − v_{0t}.
+        #   Here, we can difference the f_j’s as in (5). You should get something like (xtran[row1,:].-xtran[row0,:])'*FV[row0:row0+xbin-1,B[i]+1,t+1]
+        # • Finally, create the choice probabilities for choosing each option as written in (7) and then create the
+        #   log likelihood according to the summation in (6).
+
+        # Second loop: form the likelihood given the future values implied by the previous α guess
+        # Here, we will take the state-specifc FV's calculated in the first loop
+        # But we will only use in the likelihood those state value that are actually visited
+        like = 0 # GG: Initialize the log likelihood value to be 0.
+        for i=1:N
+            # GG: vector of indices in the ACTUAL DATA (no replacement)
+            row0 = (Zstate[i]-1)*xbin+1 # this is the same argument as the index of xtran in v0 above, but we use the actual Z
+            #z2   = (Zstate[i]-1)*xbin+1 # this is the same argument as the 1st part of the 1st index of the FV array above, but using actual Z
+            #z3   = z2+xbin-1            # this is the same argument as the 2nd part of the 1st index of the FV array above, but using actual Z
+            for t=1:T
+                # GG: vector of indices in the ACTUAL DATA (replacement)
+                row1  = Xstate[i,t] + (Zstate[i]-1)*xbin                                                                # this is the same as row in the first loop, except we use the actual X and Z
+                v1    = α[1] + α[2]*X[i,t] + α[3]*B[i] + (xtran[row1,:].-xtran[row0,:])⋅FV[row0:row0+xbin-1,B[i]+1,t+1] # using the same formula as v1 above, except we use observed values of X and B, and we difference the transitions
+                dem   = 1 + exp(v1)
+                like -= ( (Y[i,t]==1)*v1 ) - log(dem) # negative of the binary logit likelihood
+            end
+        end
+        return like
+    end
 
     #:::::::::::::::::::::::::::::::::::::::::::::::::::
     # Question 3e: Wrap all of the code you wrote in (c) and (d) into a function and set up the function so that it can be
@@ -227,11 +287,33 @@ I will walk you through speciﬁc steps for how to do this
     # the ﬁrst argument be the θ vector that we are trying to estimate
     #:::::::::::::::::::::::::::::::::::::::::::::::::::
 
+    # GG: See row 213
+
+    θ_start = rand(3)
+    θ_true  = [2; -.15; 1]
+    # how long to evaluate likelihood function once?
+    println("Timing (twice) evaluation of the likelihood function")
+    @time likebus(θ_start,0.9,Y,B,N,T,X,Z,Zstate,Xstate,xtran,zbin,xbin,xval)
+    @time likebus(θ_start,0.9,Y,B,N,T,X,Z,Zstate,Xstate,xtran,zbin,xbin,xval)
+    # estimate likelihood function
+    θ̂_optim = optimize(a -> likebus(a,0.9,Y,B,N,T,X,Z,Zstate,Xstate,xtran,zbin,xbin,xval), θ_true, LBFGS(), Optim.Options(g_tol = 1e-5, iterations=100_000, show_trace=true))
+    θ̂_ddc = θ̂_optim.minimizer
+    println(θ̂_ddc)
+
+    return nothing
+end
+
     #:::::::::::::::::::::::::::::::::::::::::::::::::::
     # Question 3f: On the same line as the function, prepend the function declaration with the macros so that your code
     # says @views @inbounds function myfun() rather than function myfun(). This will give you more performant code.
     # On my machine, it cut the computation time in half.
     #:::::::::::::::::::::::::::::::::::::::::::::::::::
+
+    # GG: See row 213
+    # @inbounds turns off dimensions checkin by Julia; consumes time, if your sure comformability is right you can turn it off to save time
+    # @views treats items as immutable, makes things more efficiently
+    # GG: You gotta make sure your functions work firt. Then you use these options to make things faster
+    # GG: in Matlab it takes about 25mins; in Julia only 5mins; if we used the TwiceDifferentiable object, it'd be even faster
 
 
 #:::::::::::::::::::::::::::::::::::::::::::::::::::
